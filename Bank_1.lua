abe6ee86d4ea47f242c9257d208191c589e28046
Anton Bolte <bolteanton22@gmail.com>
2026-01-17 10:24:21 +0100

    refactor: streamline account management and remove unused config file

--- Bank_1.lua
+++ Bank_1.lua
@@ -1,28 +1,49 @@
-print("startup...")
-mainPath = "Bank"
-configName = "testconfig.txt"
-configUrl = "https://raw.githubusercontent.com/AntonBolte/CC-Bank-System/refs/heads/main/config.txt"
+-- Bank_1.lua
 
-function download(url, filePath)
-  local content = http.get(url).readAll()
-  if not content then
-    error("[ERROR] could not connect to website!")
-  end
-  file = fs.open(filePath, "w")
-  file.write(content)
-  file.close()
+function GetFileData(path)
+    find = find(path)
+    if find == "" then
+        return "File not found"
+    end
+
+    print("Getting data from " .. path)
+    local f = fs.open(path, "r")
+    local data = textutils.unserialize(f.readAll())
+    f.close()
+    return data
 end
 
-if fs.exists(mainPath) == false
-    then print("[WARN] directory '" .. mainPath .. "' could not me found in Root!")
-         fs.makeDir("Bank")
-         print("[INFO] directory '" .. mainPath .. "' was created")
-    else print("[INFO] directory '" .. mainPath .. "' exists already")
-    end
+function WriteFileData(path, data)
+    print("Writing data to " .. path)
+    local f = fs.open(path, "w")
+    f.write(textutils.serialize(data))
+    f.close()
+end
 
-if fs.exists(mainPath .. "/" .. configName) == false
-    then print("[WARN] config file could not be found at:\n'" .. mainPath .. "/" .. configName .. "' !")
-         download(configUrl , mainPath .. "/" .. configName)
-         print("[INFO] file '" .. mainPath .. "/" .. configName .. "' was created")
-    else print("[INFO] file '" .. mainPath .. "/" .. configName .. "' exists already")
+function Add(name, amount)
+    local data = GetFileData("Bank/Accounts/" .. name .. ".txt")
+    if data == "File not found" then
+        return "Account does not exist"
     end
+    data.Balance = data.Balance + tonumber(amount)
+    WriteFileData("Bank/Accounts/" .. name .. ".txt", data.Balance)
+end
+
+print("Enter command (e.g., 'add Alice 100'):")
+local input = io.read():match("^%s*(.-)%s*$")
+local action, name, amount = string.match(input, "(%a+)%s+(%a+)%s+(%d+)")
+
+print("Action: " .. action)
+print("Account Name: " .. name)
+print("Amount: " .. amount)
+
+if action == "add" then
+    local result = Add(name, amount)
+    if result then
+        print(result)
+    else
+        print("Added " .. amount .. " to " .. name .. "'s account.")
+    end
+else
+    print("Unknown action: " .. action)
+end
